19ec0a9b89947f15a60a80a21e5b33176a2dae7b
darrenzhang <darrenzhang@91app.com>
2017-11-11 10:52:43 +0800

    Fail 2sec test case

--- Codewars_ReverseListOrder/UnitTest1.cs
+++ Codewars_ReverseListOrder/UnitTest1.cs
@@ -9,9 +9,17 @@ namespace Codewars_ReverseListOrder
         [TestMethod]
         public void Input_1_Should_Be_1()
         {
-            var actual = Kata.ReverseList(new List<int> {1});
-            var expected = new List<int> {1};
-            CollectionAssert.AreEqual(expected,actual);
+            var actual = Kata.ReverseList(new List<int> { 1 });
+            var expected = new List<int> { 1 };
+            CollectionAssert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Input_1_2_Should_Be_2_1()
+        {
+            var actaul = Kata.ReverseList(new List<int> { 1, 2 });
+            var expected = new List<int> { 2, 1 };
+            CollectionAssert.AreEqual(expected, actaul);
         }
     }
 
